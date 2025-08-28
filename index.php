a77038591c78c9d1cd4dd17c33af01a824cd8f58
EeroBertling <eero.bertling@gmail.com>
2025-08-28 14:06:05 +0300

    habibi

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> 
+    <link rel="stylesheet" href="style.css">
+    <script defer src="java.js"></script>
     <title>Portfolio</title>
 </head>
 <body>
@@ -43,8 +45,44 @@
         </div>
       </div>
     </header>
-    <a class="btn btn-warning m-2 text-center" href="http://reseptit.eerber24.treok.io/etusivu">link</a><br>
-    <img src="resp.png">
-    <footer></footer>
+    
+    <!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides">
+    <div class="numbertext">1 / 3</div>
+    <img src="kuvat/resp.png" style="width:100%" alt="">
+    <div class="text">Reseptit</div>
+  </div>
+
+  <div class="mySlides">
+    <div class="numbertext">2 / 3</div>
+    <img src="kuvat/kategoria.png" style="width:100%">
+    <div class="text">Kategoriat</div>
+  </div>
+
+  <div class="mySlides">
+    <div class="numbertext">3 / 3</div>
+    <img src="kuvat/uusiresp.png" style="width:100%">
+    <div class="text">Uuden reseptin lomake</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div><br>
+<a class="btn btn-warning m-2 text-center" href="http://reseptit.eerber24.treok.io/etusivu">link</a><br>
+    <footer>
+
+    </footer>
 </body>
 </html>
